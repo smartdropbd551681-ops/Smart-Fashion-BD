ca34b8b99fef32957594aaa4dc04657bfcba0850
smartdropbd551681-ops <smartdropbd551681@gmail.com>
2026-07-31 23:48:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const SUPABASE_URL = "https://mxiqonrtzhmqnunddrkj.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ZyPvH2HEd8eMrZ9v1qwZCg_HhuaH-NW";
 
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+async function loadProducts() {
+
+  const { data, error } = await supabase
+    .from("products")
+    .select("*")
+    .order("id", { ascending: false });
+
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log(data);
+}
+
+loadProducts();
