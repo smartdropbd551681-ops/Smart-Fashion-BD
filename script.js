89c89f5977dc0e3697875e24b14328888b8e22f9
smartdropbd551681-ops <smartdropbd551681@gmail.com>
2026-08-02 17:35:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,29 @@
-const SUPABASE_URL = "https://mxiqonrtzhmqnunddrkj.supabase.co";
-const SUPABASE_KEY = "sb_publishable_ZyPvH2HEd8eMrZ9v1qwZCg_HhuaH-NW";
+
+  const SUPABASE_URL = "https://mxiqonrtzhmqnunddrkj.supabase.co";
+const SUPABASE_KEY = "তোমার Publishable Key";
+
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
 
-async function publishProduct() {
+console.log("script.js loaded");
 
-  const name = document.getElementById("name").value.trim();
-  const price = Number(document.getElementById("price").value);
-  const category = document.getElementById("category").value;
-  const description = document.getElementById("description").value.trim();
+document.addEventListener("DOMContentLoaded", () => {
 
-  const file = document.getElementById("image").files[0];
+  const form = document.getElementById("productForm");
 
-  if (!file) {
-    alert("ছবি নির্বাচন করুন");
+  if (!form) {
+    alert("productForm পাওয়া যায়নি");
     return;
   }
 
-  const fileName = Date.now() + "_" + file.name;
+  form.addEventListener("submit", async (e) => {
 
-  const { error: uploadError } = await supabase.storage
-    .from("products")
-    .upload(fileName, file);
+    e.preventDefault();
 
-  if (uploadError) {
-    alert(uploadError.message);
-    return;
-  }
+    alert("Publish Button কাজ করছে");
 
-  const image =
-    SUPABASE_URL +
-    "/storage/v1/object/public/products/" +
-    fileName;
+  });
 
-  const { error } = await supabase
-    .from("products")
-    .insert([
-      {
-        name,
-        price,
-        category,
-        description,
-        image
-      }
-    ]);
-
-  if (error) {
-    alert(error.message);
-    return;
-  }
-
-  alert("✅ Product Published Successfully");
-
-  document.getElementById("productForm").reset();
-}
-
-document
-.getElementById("productForm")
-.addEventListener("submit", function(e){
-
-  e.preventDefault();
-
-  publishProduct();
-
-});alert("script.js চলছে");
-document.getElementById("publishBtn").addEventListener("click", function () {
-    alert("Publish Button চাপা হয়েছে");
 });
