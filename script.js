e6d7d95585a363e932216fbc7bb5413a7ce3c01a
smartdropbd551681-ops <smartdropbd551681@gmail.com>
2026-08-03 19:23:43 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
-alert("script.js loaded");
-
-  const SUPABASE_URL = "https://mxiqonrtzhmqnunddrkj.supabase.co";
-const SUPABASE_KEY = "sb_publishable_ZyPvH2HEd8eMrZ9v1qwZCg_HhuaH-NW";
+const SUPABASE_URL = "https://mxiqonrtzhmqnunddrkj.supabase.co";
+const SUPABASE_KEY = "তোমার Publishable Key";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
 
-console.log("script.js loaded");
-
 document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.getElementById("productForm");
 
-  if (!form) {
-    alert("productForm পাওয়া যায়নি");
-    return;
-  }
-
   form.addEventListener("submit", async (e) => {
-
     e.preventDefault();
 
-    alert("Publish Button কাজ করছে");
+    const name = document.getElementById("name").value;
+    const price = document.getElementById("price").value;
+    const category = document.getElementById("category").value;
+    const description = document.getElementById("description").value;
+
+    const imageFile = document.getElementById("image").files[0];
+
+    if (!imageFile) {
+      alert("ছবি নির্বাচন করুন");
+      return;
+    }
+
+    const fileName = Date.now() + "_" + imageFile.name;
+
+    const { error: uploadError } = await supabase.storage
+      .from("products")
+      .upload(fileName, imageFile);
+
+    if (uploadError) {
+      alert(uploadError.message);
+      return;
+    }
+
+    const image =
+      `${SUPABASE_URL}/storage/v1/object/public/products/${fileName}`;
+
+    const { error } = await supabase
+      .from("products")
+      .insert([
+        {
+          name,
+          price,
+          category,
+          description,
+          image
+        }
+      ]);
+
+    if (error) {
+      alert(error.message);
+    } else {
+      alert("✅ Product Published Successfully");
+      form.reset();
+    }
 
   });
 
